5d54ef47af1bb1796b8a9e918684bfb1b48dea70
Amit Karpe <amitkarpe@gmail.com>
2025-08-09 16:53:27 +0800

    tested: offline

--- bamtofastq/setup.sh
+++ bamtofastq/setup.sh
@@ -2,12 +2,9 @@
 export PIPELINE=bamtofastq
 export REVISION=2.1.1
 export BUNDLE_DIR="pipe"
-#wget https://raw.githubusercontent.com/mytestlab123/offline/refs/heads/main/bamtofastq/justfile
-wget https://raw.githubusercontent.com/mytestlab123/offline/refs/heads/main/bamtofastq/prepare_bamtofastq_offline_test.sh
 nf-core pipelines download "$PIPELINE" --revision "$REVISION" --container-system none --compress none --outdir "$BUNDLE_DIR" --force
-# OR
-#just pull
 mv pipe/2_1_1 bamtofastq
 mkdir -p bamtofastq/offline
+cp -v justfile bamtofastq
 cp -v prepare_bamtofastq_offline_test.sh bamtofastq/offline/
 cp -v ENV bamtofastq/.env
